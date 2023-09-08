e16ef38f92a834d518993f192d9ef415f061a642
singhsharad529 <singhsharad529@gmail.com>
2023-09-08 16:02:19 +0530

    added controllers for update and delete recipes

--- controllers/recipe-controller.js
+++ controllers/recipe-controller.js
@@ -2,10 +2,9 @@ const createError = require("http-errors");
 const mongoose = require("mongoose");
 const Recipe = require("../models/recipe-model");
 
-//getting all categories list
+//getting all recipe list
 const getAllRecipes = async (req, res, next) => {
   try {
-    console.log("first");
     const splitStrings = req.params.category.split("-");
     const searchString = splitStrings.join(" ");
 
@@ -36,7 +35,7 @@ const addSingleRecipe = async (req, res, next) => {
   }
 };
 
-//getting all categories list
+//getting a recipe
 const singleRecipe = async (req, res, next) => {
   console.log("starting");
 
@@ -51,4 +50,90 @@ const singleRecipe = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllRecipes, addSingleRecipe, singleRecipe };
+//update a single recipe by id
+const updateRecipe = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const updates = req.body;
+    const options = { new: true };
+    const result = await Recipe.findByIdAndUpdate(id, updates, options);
+    if (!result) {
+      next(createError(404, "Recipe does not exist"));
+      return;
+    }
+    res.send(result);
+  } catch (error) {
+    console.log("Error while recipe a single category: ", error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "Invalid Recipe id"));
+      return;
+    }
+    next(error);
+  }
+};
+
+//update a single recipe by id
+const updateRecipesCategory = async (req, res, next) => {
+  try {
+    const c_id = req.params.c_id
+    const updates = req.body;
+    const options = { new: true };
+    const result = await Recipe.updateMany({ c_id: c_id }, updates, options);
+    if (!result) {
+      next(createError(404, "Recipe does not exist"));
+      return;
+    }
+    res.send(result);
+  } catch (error) {
+    console.log("Error while recipe a single category: ", error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "Invalid Recipe id"));
+      return;
+    }
+    next(error);
+  }
+};
+
+//delete a recipe by id
+const deleteRecipe = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const result = await Recipe.findByIdAndDelete(id);
+    if (!result) {
+      next(createError(404, "Recipe does not exist"));
+      return;
+    }
+    res.send(result);
+  } catch (error) {
+    console.log("Error while deleting a single recipe: ", error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "Invalid Recipe id"));
+      return;
+    }
+    next(error);
+  }
+};
+
+
+//delete all recipe of a category(by c_id)
+const deleteAllRecipeByC = async (req, res, next) => {
+  const c_id = req.params.c_id;
+  try {
+    const fieldToDelete = c_id;
+    const result = await Recipe.deleteMany({ c_id: fieldToDelete });
+    if (!result) {
+      next(createError(404, "Category does not exist"));
+      return;
+    }
+    res.send(result);
+  } catch (error) {
+    console.log("Error while deleting a all recipe: ", error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "Invalid category id"));
+      return;
+    }
+    next(error);
+  }
+};
+
+module.exports = { getAllRecipes, addSingleRecipe, singleRecipe, updateRecipe, deleteRecipe, deleteAllRecipeByC, updateRecipesCategory };
--- routes/recipe-routes.js
+++ routes/recipe-routes.js
@@ -11,4 +11,17 @@ router.post("/:category", recipeController.addSingleRecipe);
 //getting a single recipe
 router.get("/about-recipe/:id", recipeController.singleRecipe);
 
+//update a recipe
+router.patch("/about-recipe/:id", recipeController.updateRecipe);
+
+//delete a recipe
+router.delete("/about-recipe/:id", recipeController.deleteRecipe);
+
+//delete all recipe by category
+router.delete("/about-recipe/delete-all-recipe-by-category/:c_id", recipeController.deleteAllRecipeByC);
+
+//update a recipe's category details by c_id
+router.patch("/about-recipe/update-all-recipe-by-category/:c_id", recipeController.updateRecipesCategory);
+
+
 module.exports = router;
